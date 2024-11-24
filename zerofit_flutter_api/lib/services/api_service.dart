559f547db624f87fb1db8f23efb04446146ca44b
logicallaw <logicallawbio@gmail.com>
2024-11-25 08:54:19 +0900

    chore: 변수 nodeHost, nodePort 삭제 및 GET /my-image API 삭제

--- zerofit_flutter_api/lib/services/api_service.dart
+++ zerofit_flutter_api/lib/services/api_service.dart
@@ -2,35 +2,13 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
-import '../screens/home.dart'; // 홈 화면 임포트
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiService {
-  late final String nodeHost;
-  late final String nodePort;
   late final String nodeUrl;
+
   ApiService() {
-    nodeHost = dotenv.get("NODE_HOST");
-    nodePort = dotenv.get("NODE_PORT");
-    nodeUrl = 'http://$nodeHost:$nodePort';
-  }
-  // GET /my-image
-  Future<String> fetchData() async {
-    try {
-      final response = await http.get(Uri.parse('$nodeUrl/my-image'));
-      if (response.statusCode == 200) {
-        var data = json.decode(response.body);
-        return data['message'];
-      } else {
-        final error = json.decode(response.body);
-        throw Exception('Failed to load data: ${error['message']}');
-      }
-    } catch (e) {
-      print('Error fetching data: $e');
-      return 'Error fetching data';
-    }
+    nodeUrl = 'http://${dotenv.get("NODE_HOST")}:${dotenv.get("NODE_PORT")}';
   }
 
   // POST /auth/join
